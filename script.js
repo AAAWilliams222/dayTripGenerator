805626b36a74497df81e85e5e16f0e85128c2deb
Anthony D Williams <anthonywilliams222@gmail.com>
2021-08-01 11:08:07 -0500

    Added dayTripGenerator function

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ let randomTransportation = transportation[Math.floor(Math.random() * transportat
 let randomEntertainment = entertainment[Math.floor(Math.random() * entertainment.length)];
     userChoice.push(randomEntertainment);
   console.log(userChoice);
+
+  function dayTripGenerator(){
+    let destination = ["Fiji", "Ireland", "Greece", "Brazil"];
+    let randomDestination = destination[Math.floor(Math.random() * destination.length)];
+      userChoice[0] = randomDestination;
+    let restaurant = ["PF Chang's", "Outback Steakhouse", "Olive Garden", "Red Lobster"];
+    let randomRestaurant = restaurant[Math.floor(Math.random() * restaurant.length)];
+        userChoice[1] = randomRestaurant;
+    let transportation = ["Private Jet", "Tesla", "Yacht", "Mountain Bike"];
+    let randomTransportation = transportation[Math.floor(Math.random() * transportation.length)];
+        userChoice[2] = randomTransportation;;
+    let entertainment = ["Movie", "Concert", "Sporting Event", "Netflix and Hangout, lol"];
+    let randomEntertainment = entertainment[Math.floor(Math.random() * entertainment.length)];
+        userChoice[3] = randomEntertainment;;
+      console.log(userChoice);
+    }
