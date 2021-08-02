8b07136610416d74edcb4704ef6207f5be0e167f
Anthony D Williams <anthonywilliams222@gmail.com>
2021-08-02 10:20:26 -0500

    Final commit

--- script.js
+++ script.js
@@ -8,34 +8,29 @@ let entertainment = ["Movie", "Concert", "Sporting Event", "Netflix and Hangout,
 
 let randomDestination = destination[Math.floor(Math.random() * destination.length)];
   userChoice.push(randomDestination);
-
 let randomRestaurant = restaurant[Math.floor(Math.random() * restaurant.length)];
     userChoice.push(randomRestaurant);
-
 let randomTransportation = transportation[Math.floor(Math.random() * transportation.length)];
     userChoice.push(randomTransportation);
-
 let randomEntertainment = entertainment[Math.floor(Math.random() * entertainment.length)];
     userChoice.push(randomEntertainment);
   console.log(userChoice);
 
-  function dayTripGenerator(){
-    let destination = ["Fiji", "Ireland", "Greece", "Brazil"];
-    let randomDestination = destination[Math.floor(Math.random() * destination.length)];
-      userChoice[0] = randomDestination;
-    let restaurant = ["PF Chang's", "Outback Steakhouse", "Olive Garden", "Red Lobster"];
-    let randomRestaurant = restaurant[Math.floor(Math.random() * restaurant.length)];
-        userChoice[1] = randomRestaurant;
-    let transportation = ["Private Jet", "Tesla", "Yacht", "Mountain Bike"];
-    let randomTransportation = transportation[Math.floor(Math.random() * transportation.length)];
-        userChoice[2] = randomTransportation;;
-    let entertainment = ["Movie", "Concert", "Sporting Event", "Netflix and Hangout, lol"];
-    let randomEntertainment = entertainment[Math.floor(Math.random() * entertainment.length)];
-        userChoice[3] = randomEntertainment;;
-      console.log(userChoice);
-    }
 
-    let userAgrees = "yes"; 
+function dayTripGenerator(){
+
+randomDestination = destination[Math.floor(Math.random() * destination.length)];
+  userChoice[0] = randomDestination;
+randomRestaurant = restaurant[Math.floor(Math.random() * restaurant.length)];
+    userChoice[1] = randomRestaurant;
+randomTransportation = transportation[Math.floor(Math.random() * transportation.length)];
+    userChoice[2] = randomTransportation;;
+randomEntertainment = entertainment[Math.floor(Math.random() * entertainment.length)];
+    userChoice[3] = randomEntertainment;;
+  console.log(userChoice);
+}
+
+let userAgrees = "yes"; 
 let output = false;
 
 while(output === false){
